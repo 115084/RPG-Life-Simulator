f48107781347c0d3a8b65d3935caa72548baaa2c
115084 <39541819+115084@users.noreply.github.com>
2018-07-20 00:34:41 +0000

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,45 @@
-﻿Public Class Form1
+Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+        Dim exp As Integer
+
+
+        Dim lvl As Integer
+
+
+        Dim currentExp As Integer
+
+
+
+　
+
+
+　
+
+
+    End Sub
+
+
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+
+
+    End Sub
+
+
+
+    Private Sub btnHomework_Click(sender As Object, e As EventArgs) Handles btnHomework.Click
+
+
+        currentexp = currentexp + 1
+
+
+    End Sub
+
 
 End Class
