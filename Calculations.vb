3c78d0afce54134619672325603a6a473d06d77f
115084 <39541819+115084@users.noreply.github.com>
2018-08-01 12:32:18 +1000

    Update Calculations.vb

--- Calculations.vb
+++ Calculations.vb
@@ -1,3 +1,42 @@
-﻿Public Class Calculations
+Public Class Calculations
 
+    Public Sub Calculations()
+    End Sub
+
+    Public Sub updateProgressBar(ByVal lvl As Single)
+
+
+        If lvl = 0 Then
+            Form1.progressBar.Minimum = 0
+            Form1.progressBar.Maximum = 4
+        End If
+        If lvl = 1 Then
+            Form1.progressBar.Minimum = 4
+            Form1.progressBar.Maximum = 16
+        End If
+        If lvl = 2 Then
+            Form1.progressBar.Minimum = 16
+            Form1.progressBar.Maximum = 36
+        End If
+        If lvl = 3 Then
+            Form1.progressBar.Minimum = 36
+            Form1.progressBar.Maximum = 64
+        End If
+        If lvl = 4 Then
+            Form1.progressBar.Minimum = 64
+            Form1.progressBar.Maximum = 100
+        End If
+        If lvl = 5 Then
+            Form1.progressBar.Minimum = 100
+            Form1.progressBar.Maximum = 144
+        End If
+        If lvl = 6 Then
+            Form1.progressBar.Minimum = 144
+            Form1.progressBar.Maximum = 196
+        End If
+        If lvl > 6 Then
+            Form1.progressBar.Minimum = 196
+            Form1.progressBar.Maximum = 250
+        End If
+    End Sub
 End Class
