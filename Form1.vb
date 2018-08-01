88d24e79b5b7b9a96c72409e711af1f897db1072
115084 <39541819+115084@users.noreply.github.com>
2018-08-01 12:14:31 +1000

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -3,87 +3,87 @@ Public Class Form1
     Dim exp As Integer
     Dim lvl As Single
     Dim displayLvl As Integer
+    Dim levelingSystem As String
+    Dim calculator As Calculations = New Calculations()
+    Dim sb As String
 
 
 
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub Form1_Load(sender As Object, e As EventArgs)
         currentexp = 0
         exp = 0
+
     End Sub
-    Private Sub btnHomework_Click(sender As Object, e As EventArgs) Handles btnHomework.Click, progressBar.Click
+    Private Sub btnHomework_Click_1(sender As Object, e As EventArgs) Handles btnHomework.Click
+
+        txtCurrentExp.Text = currentexp
+        lvl = Math.Floor((Math.Sqrt(currentexp) / 2))
+        progressBar.Value = currentexp
+        calculator.updateProgressBar(lvl)
+        txtLevel.Text = lvl
+
+        imgTro.Visible = True
+        imgExc.Visible = False
+        imgEye.Visible = False
+
+
+        For Each item In clistTime.CheckedItems
+            sb = (item.Substring(0, 2))
+            Convert.ToInt32(sb)
+        Next
+        MessageBox.Show(sb.ToString())
+
+        If sb = 15 Then
+            currentexp = currentexp + 1
+        End If
+        If sb = 30 Then
+            For exp As Integer = 1 To 2
+                currentexp = currentexp + Math.Floor((exp / 2))
+            Next
+        End If
+        If sb = 60 Then
+            For exp As Integer = 1 To 3
+                currentexp = currentexp + Math.Floor((exp / 2))
+            Next
+        End If
+    End Sub
+
+    Private Sub btnStudy_Click_1(sender As Object, e As EventArgs) Handles btnStudy.Click
         currentexp = currentexp + 1
         txtCurrentExp.Text = currentexp
         lvl = Math.Floor((Math.Sqrt(currentexp) / 2))
-        txtLevel.Text = lvl
         progressBar.Value = currentexp
+        calculator.updateProgressBar(lvl)
+        txtLevel.Text = lvl
 
+        imgTro.Visible = False
+        imgExc.Visible = True
+        imgEye.Visible = False
 
+        For Each item In clistTime.CheckedItems
+            sb = (item.Substring(0, 2))
+        Next
+        MessageBox.Show(sb.ToString())
     End Sub
 
-    Private Sub btnStudy_Click(sender As Object, e As EventArgs) Handles btnStudy.Click
+    Private Sub btnExercise_Click_1(sender As Object, e As EventArgs) Handles btnExercise.Click
         currentexp = currentexp + 1
         txtCurrentExp.Text = currentexp
+        progressBar.Value = currentexp
         lvl = Math.Floor((Math.Sqrt(currentexp) / 2))
         txtLevel.Text = lvl
-        progressBar.Value = currentexp
 
+        imgTro.Visible = False
+        imgExc.Visible = False
+        imgEye.Visible = True
 
+        For Each item In clistTime.CheckedItems
+            sb = (item.Substring(0, 2))
+        Next
+        MessageBox.Show(sb.ToString())
     End Sub
 
-    Private Sub btnExercise_Click(sender As Object, e As EventArgs) Handles btnExercise.Click
-
-
-        currentexp = currentexp + 1
-        txtCurrentExp.Text = currentexp
-        lvl = Math.Floor((Math.Sqrt(currentexp) / 2))
-        txtLevel.Text = lvl
-        progressBar.Value = currentexp
-
-    End Sub
-
-    Private Sub progressBar_MouseClick(sender As Object, e As EventArgs) Handles progressBar.MouseClick
-        If lvl = 0 Then
-            progressBar.Minimum = 0
-            progressBar.Maximum = 4
-        End If
-        If lvl = 1 Then
-            progressBar.Minimum = 4
-            progressBar.Maximum = 16
-        End If
-        If lvl = 2 Then
-            progressBar.Minimum = 16
-            progressBar.Maximum = 36
-        End If
-        If lvl = 3 Then
-            progressBar.Minimum = 36
-            progressBar.Maximum = 64
-            progressBar.Value = currentexp
-        End If
-        If lvl = 4 Then
-            progressBar.Minimum = 64
-            progressBar.Maximum = 100
-            progressBar.Value = currentexp
-        End If
-        If lvl = 5 Then
-            progressBar.Minimum = 100
-            progressBar.Maximum = 144
-            progressBar.Value = currentexp
-        End If
-        If lvl = 6 Then
-            progressBar.Minimum = 144
-            progressBar.Maximum = 196
-            progressBar.Value = currentexp
-        End If
-        If lvl > 6 Then
-            progressBar.Minimum = 196
-            progressBar.Maximum = 250
-            progressBar.Value = currentexp
-        End If
-    End Sub
-
-    Private Sub btnLevelUp_Click(sender As Object, e As EventArgs) Handles btnLevelUp.Click
-
-
+    Private Sub clistTime_SelectedIndexChanged(sender As Object, e As EventArgs) Handles clistTime.SelectedIndexChanged
+        
     End Sub
 End Class
