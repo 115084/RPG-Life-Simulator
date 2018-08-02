60aca99d05765314f1516c04187c7d836d9455ae
115821 <41351579+115821@users.noreply.github.com>
2018-08-03 09:58:23 +1000

    Update Calculations.vb

--- Calculations.vb
+++ Calculations.vb
@@ -9,6 +9,7 @@ Public Class Calculations
         If lvl = 0 Then
             Form1.progressBar.Minimum = 0
             Form1.progressBar.Maximum = 4
+            Form1.imgUnr.Visible = True
         End If
         If lvl = 1 Then
             Form1.progressBar.Minimum = 4
@@ -17,6 +18,8 @@ Public Class Calculations
         If lvl = 2 Then
             Form1.progressBar.Minimum = 16
             Form1.progressBar.Maximum = 36
+            Form1.imgUnr.Visible = False
+            Form1.imgBro.Visible = True
         End If
         If lvl = 3 Then
             Form1.progressBar.Minimum = 36
@@ -25,14 +28,20 @@ Public Class Calculations
         If lvl = 4 Then
             Form1.progressBar.Minimum = 64
             Form1.progressBar.Maximum = 100
+            Form1.imgBro.Visible = False
+            Form1.imgSil.Visible = True
         End If
         If lvl = 5 Then
             Form1.progressBar.Minimum = 100
             Form1.progressBar.Maximum = 144
+            Form1.imgSil.Visible = False
+            Form1.imgGol.Visible = True
         End If
         If lvl = 6 Then
             Form1.progressBar.Minimum = 144
             Form1.progressBar.Maximum = 196
+            Form1.imgGol.Visible = False
+            Form1.imgPla.Visible = True
         End If
         If lvl > 6 Then
             Form1.progressBar.Minimum = 196
