c7ba7bc9b658b83c33bab351cf299abf6cbfb895
115084 <39541819+115084@users.noreply.github.com>
2018-08-03 10:04:43 +1000

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,62 +1,112 @@
 Public Class Form1
-    Dim currentexp As Integer
-    Dim exp As Integer
+    Dim currentexp As Integer = 0
     Dim lvl As Single
     Dim displayLvl As Integer
     Dim levelingSystem As String
     Dim calculator As Calculations = New Calculations()
     Dim sb As String
+    Dim FILE_NAME As String = My.Application.Info.DirectoryPath + "\db.txt"
 
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If System.IO.File.Exists(FILE_NAME) = True Then
+            Dim fileReader As System.IO.StreamReader
+            fileReader = My.Computer.FileSystem.OpenTextFileReader("\db.txt")
+            Dim stringReader As String
+            stringReader = fileReader.ReadLine()
+            currentexp = stringReader
+        Else
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs)
-        currentexp = 0
-        exp = 0
+            MessageBox.Show("Exp file does not exist, please download db.txt")
+
+        End If
+
+        imgUnr.Visiblity = True
 
     End Sub
-    Private Sub btnHomework_Click_1(sender As Object, e As EventArgs) Handles btnHomework.Click
-        currentexp = currentexp + 1
-        txtCurrentExp.Text = currentexp
-        lvl = Math.Floor((Math.Sqrt(currentexp) / 2))
-        progressBar.Value = currentexp
-        calculator.updateProgressBar(lvl)
-        txtLevel.Text = lvl
 
-        imgTro.Visible = True
+    Private Sub btnHomework_Click_1(sender As Object, e As EventArgs) Handles btnHomework.Click
+        currentexp = currentexp + 1 'each time this button is pressed add 1exp
+        txtCurrentExp.Text = currentexp 'displaying the exp on the form
+        lvl = Math.Floor((Math.Sqrt(currentexp) / 2)) 'the level will equal 0.75 and will round up due to using integer, so i used some round down code
+        progressBar.Value = currentexp 'setting the progress bar to the current value of exp
+        calculator.updateProgressBar(lvl) 'sends the lvl to the calculations class, then it will work out how much is need
+        txtLevel.Text = lvl 'display lvl on form
+
+        ImgTro.Visible = True 'this is showing the img for when you press this button
         imgExc.Visible = False
         imgEye.Visible = False
 
+        If System.IO.File.Exists(FILE_NAME) = True Then
 
+            Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+
+            objWriter.Write(txtCurrentExp.Text)
+            objWriter.Close()
+            MessageBox.Show("Text written to file")
+
+        Else
+
+            MessageBox.Show("File Does Not Exist")
+
+        End If
     End Sub
+
 
     Private Sub btnStudy_Click_1(sender As Object, e As EventArgs) Handles btnStudy.Click
-        currentexp = currentexp + 1
+        currentexp = currentexp + 1 'the code is the same here as is for btnHomework_Click_1
         txtCurrentExp.Text = currentexp
         lvl = Math.Floor((Math.Sqrt(currentexp) / 2))
         progressBar.Value = currentexp
         calculator.updateProgressBar(lvl)
         txtLevel.Text = lvl
 
-        imgTro.Visible = False
-        imgExc.Visible = True
+        ImgTro.Visible = False
+        imgExc.Visible = True 'this is showing the img for when you press this button
         imgEye.Visible = False
 
+        If System.IO.File.Exists(FILE_NAME) = True Then
+
+            Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+
+            objWriter.Write(txtCurrentExp.Text)
+            objWriter.Close()
+            MessageBox.Show("Text written to file")
+
+        Else
+
+            MessageBox.Show("File Does Not Exist")
+
+        End If
     End Sub
+
 
     Private Sub btnExercise_Click_1(sender As Object, e As EventArgs) Handles btnExercise.Click
-        currentexp = currentexp + 1
+        currentexp = currentexp + 1 'the code is the same here as is for btnHomework_Click_1
         txtCurrentExp.Text = currentexp
-        progressBar.Value = currentexp
         lvl = Math.Floor((Math.Sqrt(currentexp) / 2))
+        progressBar.Value = currentexp
+        calculator.updateProgressBar(lvl)
         txtLevel.Text = lvl
 
-        imgTro.Visible = False
+        ImgTro.Visible = False
         imgExc.Visible = False
-        imgEye.Visible = True
+        imgEye.Visible = True 'this is showing the img for when you press this button
 
+        If System.IO.File.Exists(FILE_NAME) = True Then
+
+            Dim objWriter As New System.IO.StreamWriter(FILE_NAME)
+
+            objWriter.Write(txtCurrentExp.Text)
+            objWriter.Close()
+            MessageBox.Show("Text written to file")
+
+        Else
+
+            MessageBox.Show("File Does Not Exist")
+
+        End If
     End Sub
 
-    Private Sub clistTime_SelectedIndexChanged(sender As Object, e As EventArgs)
 
-    End Sub
 End Class
